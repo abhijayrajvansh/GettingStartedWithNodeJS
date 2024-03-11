df6e647c23723901ff24072e92b27ced4156474d
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-03-11 22:03:52 +0530

    update: sync and async file handling

--- filehandling/file.js
+++ filehandling/file.js
@@ -1,8 +1,17 @@
 const fs = require("fs")
 
-
 // to create a file in a synchronous manner..
-fs.writeFileSync("./syncFile.txt", "created using sync fn")
+const res = fs.writeFileSync("./syncFile.txt", "created using sync fn")
 
-// to create a file in a asynchronous manner..
+// to create a file in a asynchronous manner.. always require a callback function to handle error and result
 fs.writeFile("./asyncFile.txt", "created using async fn", (err) => {})
+
+// again using asynch function with a callback
+fs.readFile("./contact.txt", "utf-8", (err, result) => {
+  if(err) {
+    console.log("error", err)
+  }
+  else {
+    console.log(result)
+  }
+})
